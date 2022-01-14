4f1e6690442f549a0308d80915a18a077fba7e09
LAPTOP-UIPBLUVT\franc <franck-benault@orange.fr>
2022-01-14 21:19:41 +0100

    [i18n] complete test find

--- L002-nv-i18n/src/test/java/net/franckbenault/learning/country/CountryListTestCase.java
+++ L002-nv-i18n/src/test/java/net/franckbenault/learning/country/CountryListTestCase.java
@@ -2,6 +2,7 @@ package net.franckbenault.learning.country;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +10,13 @@ import com.neovisionaries.i18n.CountryCode;
 
 public class CountryListTestCase {
 	
+	
+	private String describeCountry(CountryCode country) {
+		return country+ " "+ country.getAlpha3()+ " "+country.getNumeric()+ " "+ country.getName();
+	}
+	
 	@Test
-	void testCountryCollection() {
+	public void testCountryCollection() {
 		
 		CountryCode[] countries = CountryCode.values();
 		assertNotNull(countries);
@@ -18,8 +24,39 @@ public class CountryListTestCase {
 		assertTrue(countries.length>250);
 		
 		for(CountryCode country : countries) {
-			System.out.println(country+ " "+ country.getAlpha3()+ " "+country.getNumeric()+ " "+ country.getName());
+			System.out.println(describeCountry(country));
 		}
+
+	}
+	
+	@Test
+	public void testFindCountryByAlpha3Code() {
+		
+		CountryCode fr = CountryCode.getByAlpha3Code("FRA");
+		assertNotNull(fr);
+		System.out.println(describeCountry(fr));	
+	
+		assertNull(CountryCode.getByAlpha2Code("ZZZ"));
+	}
+	
+	@Test
+	public void testFindCountryByAlpha2Code() {
+		
+		CountryCode fr = CountryCode.getByAlpha2Code("FR");
+		assertNotNull(fr);
+		System.out.println(describeCountry(fr));	
+	
+		assertNull(CountryCode.getByAlpha2Code("ZZ"));
+	}
+	
+	@Test
+	public void testFindCountryByCode() {
+		
+		CountryCode fr = CountryCode.getByCode(250);
+		assertNotNull(fr);
+		System.out.println(describeCountry(fr));	
+	
+		assertNull(CountryCode.getByCode(999));
 	}
 
 }
